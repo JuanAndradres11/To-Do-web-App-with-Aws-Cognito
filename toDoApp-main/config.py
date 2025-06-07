003c72afff36b57130936658fe57b2f42b407191
Juan Andrades <138751379+JuanAndradres11@users.noreply.github.com>
2025-06-07 22:31:27 +0530

    Update config.py

--- toDoApp-main/config.py
+++ toDoApp-main/config.py
@@ -1,10 +1,10 @@
 import os
 
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "juan1104")
-DB_HOST = os.getenv("DB_HOST", "mydb1.crum4qey00uo.us-east-1.rds.amazonaws.com")
-DB_NAME = os.getenv("DB_NAME", "mydb1")
-DB_PORT = os.getenv("DB_PORT", "3306")  # Default MySQL port
+DB_USER = os.getenv("DB_USER", " ")
+DB_PASSWORD = os.getenv("DB_PASSWORD", " ")
+DB_HOST = os.getenv("DB_HOST", " ")
+DB_NAME = os.getenv("DB_NAME", " ")
+DB_PORT = os.getenv("DB_PORT", " ")  
 
 #DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
